085c6e568686b54306a9831e63a7fa81353ecdaf
nidianlasso <nidianlassocha@gmail.com>
2024-12-02 14:35:33 -0500

    Limipeza de la pantalla mediante configuracion de tecla C

--- calculator.py
+++ calculator.py
@@ -26,11 +26,38 @@ route_icon = "C:/Users/nidia/Documents/Cursos/Python/Calculadora/Calculadora/cal
 window.iconbitmap(route_icon)
 
 
-#MOSTRAR PANTALLA DE LA CALCULADORA
+#FUNCIONALIDADES DE LAS TECLAS
+#VARIABLE QUE MUESTRA LO SI TIENE ALGO EL VISOR
+expression = ""
+
+#VARIABLE PARA ALMACENAR EL ESTADO DEL VISOR
+state_calculator = False
+
+#ACTUALIZAR LA PANTALLA SEGUN LO QUE ESCRIBA EL USUARIO
+def pulsar_tecla(tecla):
+    global expression
+
+    expression+= str(tecla)
+    visor_texto.set(expression)
+
+
+def limpiar():
+    global expression, state_calculator
+
+    expression = ""
+    visor_texto.set(expression)
+    state_calculator= False
+
+
+for i in range (5):
+    window.grid_rowconfigure(i, weight=1)
+for i in range (4):
+    window.grid_columnconfigure(i, weight=1)
+
 
 #INTANCIAR LA CLASE STRINGVAR
-text_screen = tk.StringVar() #Permite enlazar el visor con los botones para que se actualice
-text_screen =tk.Entry(window, textvariable=text_screen,
+visor_texto = tk.StringVar() #Permite enlazar el visor con los botones para que se actualice
+visor =tk.Entry(window, textvariable=visor_texto,
                       font=('Helvetica', 24),
                        bd=10,
                         insertwidth=4, #Cantidad de digitos que se pueden ingresar
@@ -38,10 +65,7 @@ text_screen =tk.Entry(window, textvariable=text_screen,
                          borderwidth=4,
                          justify='right')
 
-text_screen.grid(row=0, column=0, columnspan=4)
-# text_screen.pack(padx=30, pady=30)
-print(f"Esto es lo que se recibe de la pantalla {text_screen.get()}")
-text_screen.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
+visor.grid(row=0, column=0, columnspan=4)
 
 botones = [
     ('7', 1, 0,), ('8', 1, 1), ('9', 1, 2), ('/', 1, 3),
@@ -49,24 +73,33 @@ botones = [
     ('1', 3, 0), ('2', 3, 1), ('3', 3, 2), ('-', 3, 3),
     ('0', 4, 0), (',', 4, 1), ('C', 4, 2), ('+', 4, 3),
 ]
+
+#FUNCION PARA ACTUALIZAR LA EXPRESION
+
 for (texto, fila, columna) in botones:
-    if texto == ',' or texto == 'C':
-        btn = tk.Button(window, text=texto, bg='#075396', fg='#F9F8FE', font=('Helvetica', 18), borderwidth=4)
-    elif texto == '/' or texto == '*' or texto =='-' or texto == '+':
-        btn = tk.Button(window, text=texto, bg='#907BC7', fg='#F9F8FE', font=('Helvetica', 18), borderwidth=4)
+
+    if texto == 'C':
+        comando = limpiar
     else:
-        btn = tk.Button(window, text=texto, bg="#A4C4FA", fg='#0A010C', font=('Helvetica', 18), borderwidth=4)
-    btn.grid(row=fila, column=columna, padx=5, pady=5, sticky="nsew") 
-    
+        comando = lambda x=texto: pulsar_tecla(x)
+
+    tk.Button(window, text=texto, padx=20, pady=20, font=('Helvetica', 18),
+    command=comando).grid(row=fila, column=columna, sticky='nsew')
+            # if texto == ',' or texto == 'C':
+            #     btn = tk.Button(window, text=texto, bg='#075396', fg='#F9F8FE', font=('Helvetica', 18), borderwidth=4)
+            # elif texto == '/' or texto == '*' or texto =='-' or texto == '+':
+            #     btn = tk.Button(window, text=texto, bg='#907BC7', fg='#F9F8FE', font=('Helvetica', 18), borderwidth=4)
+            # else:
+            #     btn = tk.Button(window, text=texto, bg="#A4C4FA", fg='#0A010C', font=('Helvetica', 18), borderwidth=4)
+            # btn.grid(row=fila, column=columna, padx=5, pady=5, sticky="nsew") 
+        
 btn_equal = tk.Button(window, text='=',
                        width=40,
                        height=2,
                        bd=10,
                        borderwidth=4,
                        justify='center',
-                       ).grid(row=5, column=0, columnspan=4)
-
-
+                       command=comando).grid(row=5, column=0, columnspan=4)
 
 
 
